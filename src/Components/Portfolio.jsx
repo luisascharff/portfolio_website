f4239e70afd5a35010fcccb2031759f6f8538ea5
Luisa <123731584+luisascharff@users.noreply.github.com>
2024-02-25 11:12:30 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/dust.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,29 +31,17 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Explainable AI Demo Application",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Visualization of Layer-Wise Relevance Propagation (LRP) to explain the predictions of a pre-trained neural network.",
+    url: "https://github.com/luisascharff/xai_demo",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Spotify Clone",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+    "A clone of the Spotify web player. Built with Node.js",
+    url: "https://github.com/luisascharff/spotify-clone",
+  }
 ];
 
 const Portfolio = () => {
